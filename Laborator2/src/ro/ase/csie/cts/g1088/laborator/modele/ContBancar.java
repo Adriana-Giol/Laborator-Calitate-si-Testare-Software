32b17cfda347dbbb6f9fe1746abd1a4800f7f908
Adriana-Giol <gioladriana5@gmail.com>
2021-03-04 13:54:04 +0200

    Seminar 2 - Rezolvare Erori din clasa ContDepozit

--- Laborator2/src/ro/ase/csie/cts/g1088/laborator/modele/ContBancar.java
+++ Laborator2/src/ro/ase/csie/cts/g1088/laborator/modele/ContBancar.java
@@ -5,13 +5,13 @@ public abstract class ContBancar extends Cont{
 	protected double balanta;
 	protected String iban;
 	
-	
+	//Implementare Constructor cu Parametri
 	public ContBancar(double balanta, String iban) {
 		this.balanta = balanta;
 		this.iban = iban;
 	}
 
-	
+	//Supradefinirea metodei getBalanta din clasa Cont
 	@Override
 	public double getBalanta() {
 		return this.balanta;
