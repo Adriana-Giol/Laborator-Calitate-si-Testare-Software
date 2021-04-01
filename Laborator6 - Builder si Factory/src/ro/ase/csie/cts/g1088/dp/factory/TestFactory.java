362a2a6efa976afa027ce67b0047e4360a315c20
Adriana-Giol <gioladriana5@gmail.com>
2021-04-01 13:15:20 +0300

    Seminar 6 - Implementare Factory - Impelentare clasa Caracter Abstract, clasa Caracter Disney, clasa Caracter Marvel

---
+++ Laborator6 - Builder si Factory/src/ro/ase/csie/cts/g1088/dp/factory/TestFactory.java
@@ -0,0 +1,16 @@
+package ro.ase.csie.cts.g1088.dp.factory;
+
+import java.util.ArrayList;
+
+public class TestFactory {
+
+	public static void main(String[] args) {
+		
+		CaracterAbstract mickeyMouse = new CaracterDisney("rosu", "Mickey Mouse");
+		CaracterAbstract spierMan = new CaracterMarvel("Spiderman",100);
+		
+		ArrayList<CaracterAbstract> caractere = new ArrayList<>();
+
+	}
+
+}
