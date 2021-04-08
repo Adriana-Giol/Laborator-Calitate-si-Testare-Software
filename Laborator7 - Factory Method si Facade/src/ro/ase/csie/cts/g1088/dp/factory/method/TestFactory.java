0163c3e252f43a57c60b2f345bf5c6b7b3f1efd9
Adriana-Giol <gioladriana5@gmail.com>
2021-04-08 12:25:07 +0300

    Seminar 7 - Factory Abstract

--- Laborator7 - Factory Method si Facade/src/ro/ase/csie/cts/g1088/dp/factory/method/TestFactory.java
+++ Laborator7 - Factory Method si Facade/src/ro/ase/csie/cts/g1088/dp/factory/method/TestFactory.java
@@ -4,7 +4,14 @@ public class TestFactory {
 
 	public static void main(String[] args) {
 		
-		boolean 
+		boolean isModJoc4Kids=true;
+		
+		if(isModJoc4Kids) {
+			//dezvoltare doar cu caractere 4kids
+		}
+		else {
+			//dezvoltare doar cu caratere normal
+		}
 
 	}
 
