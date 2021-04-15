d20a3e1aed56c6a889c51f90e006f57a74c0aa06
Adriana-Giol <gioladriana5@gmail.com>
2021-04-15 12:36:08 +0300

    Seminar 8 - Implementare Decorator (inainte de pauza cu Bug)

--- Laborator8 - Decorator - Composite - Flyweight/src/ro/ase/csie/cts/g1088/dp/decorator/DecoratorErouAbstract.java
+++ Laborator8 - Decorator - Composite - Flyweight/src/ro/ase/csie/cts/g1088/dp/decorator/DecoratorErouAbstract.java
@@ -7,7 +7,23 @@ public abstract class DecoratorErouAbstract extends SuperErou{
 	public DecoratorErouAbstract(SuperErou erou) {
 		super(erou.nume, erou.puncteViata);
 		this.erou = erou;
-	
+	}
+
+	@Override
+	public void alearga() {
+		this.erou.alearga();
+		
+	}
+
+	@Override
+	public void esteLovit(int puncte) {
+		this.erou.esteLovit(puncte);
+		
+	}
+
+	@Override
+	public void seVindeca(int puncte) {
+		this.erou.seVindeca(puncte);
 		
 	}
 
--- Laborator8 - Decorator - Composite - Flyweight/src/ro/ase/csie/cts/g1088/dp/decorator/TestDecorator.java
+++ Laborator8 - Decorator - Composite - Flyweight/src/ro/ase/csie/cts/g1088/dp/decorator/TestDecorator.java
@@ -4,10 +4,26 @@ public class TestDecorator {
 
 	public static void main(String[] args) {
 		
-		Dragon dragon = new Dragon("Dragonul albastru", 1000);
+		SuperErou dragon = new Dragon("Dragonul albastru", 1000);
 		dragon.alearga();
-		dragon.esteLovit(200);
+		dragon.esteLovit(800);
 		dragon.seVindeca(100);
+		
+		SuperErou dragon2 = new Dragon("Dragonul alben", 1000);
+		dragon2.alearga();
+		dragon2.esteLovit(200);
+		dragon2.seVindeca(100);
+		
+		//aplicam decoratori pe primul dragon - un singur obiect se va modifica
+		//SuperErou dragonCuArmura = new DecoratorArmura(dragon, 300);
+		//dragonCuArmura.esteLovit(400);
+		
+		dragon = new DecoratorArmura(dragon, 300);
+		dragon.esteLovit(400);
+		
+		dragon = new DecoratorEsteRanit(dragon);
+		dragon.esteLovit(600);
+		dragon.alearga();
 	}
 
 }
